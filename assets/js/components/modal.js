e4d37bbcd3514d23cbd85a409b0d36fe06be5d24
Rivetti <thomasrivetti87@gmail.com>
2021-10-26 16:04:48 +0200

    main js et modal js créée ainsi que mise à jour du photographerjson

---
+++ assets/js/components/modal.js
@@ -0,0 +1,4 @@
+/*regex*/
+const regexFirstName = /^[A-zÀ-ú]+[A-zÀ-ú-]?[A-zÀ-ú]+$/;
+const regexLastName = /^[A-zÀ-ú]+[A-zÀ-ú-]?[A-zÀ-ú]+$/;
+const regexEmail = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;

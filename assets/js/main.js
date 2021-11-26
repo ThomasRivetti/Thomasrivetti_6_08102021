7278139ad22369b54a90bb39a0cec0a56a3da329
Rivetti <thomasrivetti87@gmail.com>
2021-11-26 14:44:57 +0100

    revue style des éléments au survol et à la tabulation

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,7 @@ function launchModal() {
   mainWrapper.classList.add("modalFormOpenStyle");
   body.classList.add("no-scroll");
   closeModalBtn.focus();
+  closeModalBtn.classList.toggle("visible");
   console.log("open");
 }
 
@@ -38,6 +39,33 @@ function closeModal() {
   
 }
 
+// add all the elements inside modal which you want to make focusable
+const focusableElements ='button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+const firstFocusableElement = contactModal.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+const focusableContent = contactModal.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+
+document.addEventListener('keydown', function(e) {
+  let isTabPressed = e.key === 'Tab' || e.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (e.shiftKey) { // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      e.preventDefault();
+    }
+  } else { // if tab key is pressed
+    if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+      firstFocusableElement.focus(); // add focus for the first focusable element
+      e.preventDefault();
+    }
+  }
+});
+
+firstFocusableElement.focus();
 
 //checking Input Firstname
 firstNameInput.addEventListener("blur", function(){
